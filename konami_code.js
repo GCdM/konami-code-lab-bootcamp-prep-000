263ca341b515abdef9dff0c9eff08b20bf27b974
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-07 13:35:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,14 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   function konamiHandler(e) {
-    const key = parseInt(e.detail || e.which)
+    const key = parseInt(e.detail || e.which);
+    
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Konami wants to congratulate you!!!")
+      }
+    }
   }
   
   document.body.addEventListener('keydown', konamiHandler);
