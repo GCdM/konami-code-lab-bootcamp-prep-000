2caa73404c4545de0f429a0fdaae67e6fd33f513
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-07 13:30:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  let index = 0;
+  function konamiHandler(e) {
+    
+  }
   
+  document.body.addEventListener('keydown', konamiHandler);
   
-
 }
